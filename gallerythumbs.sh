c95e5020dda04cd73f1f8b50d77f853b62adbf67
Jack Hughes <digitalbricklayer@gmail.com>
2020-09-28 11:55:54 +0100

    Adds check for imagemagick in gallerythumbs.sh

--- gallerythumbs.sh
+++ gallerythumbs.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 # Script requires imagemagick
+if ! [ -x "$(command -v convert)" ]; then
+    echo "Error: imagemagick is not installed"
+    exit
+fi
 for i in `find static/image/galleries -type f ! -name "*-thumb.jpg" -name "*.jpg"`; do echo $i; if [ -f ${i%.*}-thumb.jpg ]; then continue; fi; convert $i -thumbnail 100x100 ${i%.*}-thumb.jpg; done
 for i in `find static/image/galleries -type f ! -name "*-thumb.png" -name "*.png"`; do echo $i; if [ -f ${i%.*}-thumb.png ]; then continue; fi; convert $i -thumbnail 100x100 ${i%.*}-thumb.png; done
