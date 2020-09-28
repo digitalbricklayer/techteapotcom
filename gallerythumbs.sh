5c35034fdf56f09f4d271c14400bb10cba10b383
Jack Hughes <digitalbricklayer@gmail.com>
2020-09-28 11:40:08 +0100

    Add .png file support to gallerythumbs.sh

--- gallerythumbs.sh
+++ gallerythumbs.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 for i in `find static/images/galleries -type f ! -name "*-thumb.jpg" -name "*.jpg"`; do echo $i; if [ -f ${i%.*}-thumb.jpg ]; then continue; fi; convert $i -thumbnail 100x100 ${i%.*}-thumb.jpg; done
+for i in `find static/images/galleries -type f ! -name "*-thumb.png" -name "*.png"`; do echo $i; if [ -f ${i%.*}-thumb.png ]; then continue; fi; convert $i -thumbnail 100x100 ${i%.*}-thumb.png; done
