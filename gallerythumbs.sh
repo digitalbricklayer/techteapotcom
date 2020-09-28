c1921edc856ef94347924606c85ed4a63019754f
Jack Hughes <digitalbricklayer@gmail.com>
2020-09-28 11:46:34 +0100

    Update gallerythumbs.sh

--- gallerythumbs.sh
+++ gallerythumbs.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+# Script requires imagemagick
 for i in `find static/image/galleries -type f ! -name "*-thumb.jpg" -name "*.jpg"`; do echo $i; if [ -f ${i%.*}-thumb.jpg ]; then continue; fi; convert $i -thumbnail 100x100 ${i%.*}-thumb.jpg; done
 for i in `find static/image/galleries -type f ! -name "*-thumb.png" -name "*.png"`; do echo $i; if [ -f ${i%.*}-thumb.png ]; then continue; fi; convert $i -thumbnail 100x100 ${i%.*}-thumb.png; done
