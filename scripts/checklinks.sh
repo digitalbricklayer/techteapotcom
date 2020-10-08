71446f5d71e583b06ee7fc8f660312e7377ec1b9
Jack Hughes <digitalbricklayer@gmail.com>
2020-10-08 16:02:03 +0100

    Make sure linkchecker is installed before running checklinks.sh script

--- scripts/checklinks.sh
+++ scripts/checklinks.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+# Run 'hugo server' at the same time as this script
+# Script requires linkchecker
+if ! [ -x "$(command -v linkchecker)" ]; then
+    echo "Error: linkchecker is not installed"
+    exit
+fi
+
 linkchecker --ignore-url=/livereload.js \
             --ignore-url=/dist \
             --no-status \
